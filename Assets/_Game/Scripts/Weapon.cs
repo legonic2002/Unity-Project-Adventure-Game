36ee14ac197ab321e952981c972f04346e88325e
Duy Anh <Haduyanh123@gmail.com>
2023-07-10 20:40:40 +0700

    add player,animator,animation
    player(run , attack , jum , throw)

---
+++ Assets/_Game/Scripts/Weapon.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public bool isThrow;
+    [SerializeField] private float speedThrow;
+
+    // Update is called once per frame
+    void Update()
+    {
+           if(isThrow)
+        {
+                transform.position = Vector3.MoveTowards(transform.position , transform.right , speedThrow * Time.deltaTime);
+        }
+    }
+}
